63ef1fd74a1dba13bc000f604e8c2b8b7f6b055e
Paulo Basilio <paulolorenzobasilio@yahoo.com>
2020-06-09 02:00:58 +0800

    added highlight of calendar events

--- resources/js/components/Calendar.vue
+++ resources/js/components/Calendar.vue
@@ -4,62 +4,31 @@
       <div class="row">
         <!-- form -->
         <div class="col-md-4">
-          <form>
+          <form @submit="saveEvent">
             <div class="form-group">
               <label>Event</label>
-              <input type="text" class="form-control" />
+              <input type="text" class="form-control" v-model="form.event" />
             </div>
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label>From</label>
-                <input type="text" class="form-control" />
+                <input type="date" class="form-control" v-model="form.fromDate" />
               </div>
               <div class="form-group col-md-6">
                 <label>To</label>
-                <input type="text" class="form-control" />
+                <input type="date" class="form-control" v-model="form.toDate" />
               </div>
             </div>
             <div class="form-row">
-              <div class="col">
+              <div class="col" v-for="(day,index) in days" :key="index">
                 <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Mon</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Tue</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Wed</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Thu</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Fri</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Sat</label>
-                </div>
-              </div>
-              <div class="col">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" />
-                  <label class="form-check-label">Sun</label>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    v-model="form.selectedDays"
+                    :value="day"
+                  />
+                  <label class="form-check-label">{{ day }}</label>
                 </div>
               </div>
             </div>
@@ -68,16 +37,15 @@
         </div>
         <!-- date -->
         <div class="col-md-8">
-          <h2>Jul 2018</h2>
+          <h2>Jun 2020</h2>
           <hr />
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">1 Sun</li>
-            <li class="list-group-item">2 Mon</li>
-            <li class="list-group-item">3 Tue</li>
-            <li class="list-group-item">4 Wed</li>
-            <li class="list-group-item">5 Thu</li>
-            <li class="list-group-item">6 Fri</li>
-            <li class="list-group-item">7 Sat</li>
+            <li
+              class="list-group-item"
+              v-for="(date, index) in dates"
+              :id="date.id"
+              :key="index"
+            >{{ `${date.date.getDate()} ${getNameOfDay(date.date)}` }}</li>
           </ul>
         </div>
       </div>
@@ -89,6 +57,81 @@
 import Card from "./Card";
 
 export default {
-  name: "Calendar"
+  name: "Calendar",
+  data() {
+    return {
+      dates: [],
+      form: {
+        event: null,
+        fromDate: null,
+        toDate: null,
+        selectedDays: []
+      },
+      days: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    };
+  },
+  created() {
+    this.dates = this.getDates(6, 2020);
+  },
+  methods: {
+    getDates(month, year) {
+      return new Array(31)
+        .fill("")
+        .map((v, i) => {
+          return {
+            id: `june-${i + 1}`,
+            date: new Date(year, month - 1, i + 1),
+            selected: false
+          };
+        })
+        .filter(v => v.date.getMonth() === month - 1);
+    },
+    getNameOfDay(day) {
+      return new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(day);
+    },
+    getDateRange(start, end) {
+      let dates = [];
+      const endDate = new Date(end);
+
+      for (
+        const d = new Date(start);
+        d <= endDate;
+        d.setDate(d.getDate() + 1)
+      ) {
+        const date = new Date(d);
+        date.setHours(0, 0, 0, 0);
+        dates.push(date);
+      }
+
+      return dates;
+    },
+    clearEvents() {
+      this.dates.forEach(date =>
+        document
+          .getElementById(date.id)
+          .classList.remove("list-group-item-success")
+      );
+    },
+    saveEvent(e) {
+      e.preventDefault();
+      this.clearEvents();
+
+      let selectedDateRange = this.getDateRange(
+        this.form.fromDate,
+        this.form.toDate
+      ).filter(date =>
+        this.form.selectedDays.includes(this.getNameOfDay(date))
+      );
+
+      selectedDateRange.forEach(dateRange => {
+        const date = this.dates.find(
+          date => date.date.getTime() === dateRange.getTime()
+        );
+        document
+          .getElementById(date.id)
+          .classList.add("list-group-item-success");
+      });
+    }
+  }
 };
 </script>
