87f11aea47c801493789c5dee8f57e2ced98dd94
Paulo Basilio <paulolorenzobasilio@yahoo.com>
2020-06-10 11:45:37 +0800

    added my name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Events</title>
+    <title>Appetiser Events</title>
     <link rel="stylesheet" href="dist/app.css" />
 </head>
 
 <body>
     <div id="app" class="container-fluid mt-2">
+        <h4>Paulo Lorenzo M. Basilio (paulolorenzobasilio@yahoo.com)</h4>
         <calendar></calendar>
     </div>
     <script src="dist/app.js"></script>
